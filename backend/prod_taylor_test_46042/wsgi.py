6e7bb9c50adb42e6c19f4a737871f602b33502f9
Crowdbotics <team@crowdbotics.com>
2024-02-06 20:44:53 +0000

    Initial commit

---
+++ backend/prod_taylor_test_46042/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for prod_taylor_test_46042 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "prod_taylor_test_46042.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "prod-taylor-test-46042",
+  url: "https://prod-taylor-test-46042.botics.co",
+  api: "https://prod-taylor-test-46042.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
